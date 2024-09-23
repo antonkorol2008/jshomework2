bba3f92ad54b63b3afbdec9cc03704a88111d23a
antonkorol2008 <antonkorol6r@gmail.com>
2024-09-23 20:53:54 +0300

    add.js
    h

---
+++ index.js
@@ -0,0 +1,40 @@
+let celsius = 25;
+
+let fahrenheit;
+fahrenheit = celsius * (9/5) + 32;
+console.log(celsius + "c is" +  fahrenheit + "F");
+let daysInMonth = 30;
+let hoursInMonth = daysInMonth * 24;
+ let minutesInMonthn = hoursInMonth * 60
+ console.log("У" + daysInMonth + "днях:" + hoursInMonth + "годин та" + minutesInMonthn + "хвилин" );
+
+ let health = 100;
+ let energy = 80;
+
+ health -= 20;
+ energy -= 15;
+  console.log("Рівень здоров'я гравця:" + health + "%" );
+  console.log("Рівень енергії гравця:" + energy +  "%");
+
+  let purchaseAmount = 500;
+   
+  let discountAmount = purchaseAmount * 0.9;
+
+  console.log("Cума після застосування знижки:" + discountAmount + "гривень." );
+  
+  let floatingNumber = 7.89;
+  let roundedNumber = Math.floor(floatingNumber)
+  console.log("Oкруглене число:" + roundedNumber);
+  let numberString = "123.45"
+
+  let decimalNumber = parseFloat (numberString)
+  console.log("Перетворене число:" + decimalNumber);
+   
+  let integerString = "123";
+  let integerNumber = parseInt(integerString);
+  console.log("Перетворене ціле число:" + integerNumber);
+   let number = 64;
+   let squreRoot = Math.sqrt(number);
+   console.log("Квадратний корінь з" + number + "дорівнює:" + squreRoot);
+
+   
